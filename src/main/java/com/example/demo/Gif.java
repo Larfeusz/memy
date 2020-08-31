2877f1159979a6efaea93244b4088fa10a370144
Larfeusz” <piotr.jakubiak@onet.pl>
2020-08-31 12:11:39 +0200

    display gifs in home.html

---
+++ src/main/java/com/example/demo/Gif.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data // lombok
+ // Konstruktor dwuargumentowy
+public class Gif {
+
+    private String name;
+    public String getFilePath (){
+        return "/gifs/"+name+".gif";
+    }
+
+    public Gif(String name) {
+        this.name = name;
+    }
+    // Model mam służy do przekazywania zmiennej do HTML
+
+    public static List<Gif> GIFS = new ArrayList<>();
+    // Nie możemy zrobić GIFS.add , bo zmienna jest statyczna
+    // żeby zainicjalizować zmienną statyczną (wszystkie gify z folderu "gifs" dodać do tablicy
+    // to musimy użyć bloku statycznego
+
+    static {
+        GIFS.add(new Gif("android-explosion"));
+        GIFS.add(new Gif("ben-and-mike"));
+        GIFS.add(new Gif("book-dominos"));
+        GIFS.add(new Gif("compiler-bot"));
+        GIFS.add(new Gif("cowboy-coder"));
+        GIFS.add(new Gif("infinite-andrew"));
+
+     // to powyżej, to jest taka nasza baza danych, bez bazy danych
+
+    }
+}
--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -1,14 +1,15 @@
 package com.example.demo;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String getHome(){
-
+    public String getHome(ModelMap map){
+        map.put("gifs",Gif.GIFS);
         return "home";
     }
 }
